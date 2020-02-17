81851f90a02a3e2398d74fc5bb25b9745411184a
Kunal Bhardwaj <46914529+512-kb@users.noreply.github.com>
2020-02-17 16:31:16 +0530

    Create url.js

---
+++ src/url.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line
+const webURL = "https://kb512-web.herokuapp.com";
+const apiURL = "https://kb512-api.herokuapp.com";
+// eslint-disable-next-line
+const localURL =
+  window.location.protocol + "//" + window.location.hostname + ":" + 4000;
+
+export default apiURL;
