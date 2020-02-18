097f57c9a70db5e991e61abfddb1f237cab91ebb
512-kb <kb98kb98kb@gmail.com>
2020-02-18 16:52:20 +0530

    infinite update

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -15,26 +15,26 @@ class User extends React.Component {
     this.loadUser();
     this.state = this.props.location.state;
   }
-  componentWillUpdate = async () => {
-    this.setState({ requestSent: true });
-    const info = await axios.get(URL + "/user/getInfo?_id=" + this.state._id);
-    if (info.data === "NOT FOUND") {
-      history.goBack();
-      return;
-    }
-    if (info.data.id.length > 0)
-      await axios
-        .get(URL + "/user/subscriptions/?id=" + this.state.id)
-        .then(res => {
-          info.data.existingPlan = {
-            id: res.data.plan.id,
-            name: res.data.plan.nickname,
-            sub_id: res.data.sub_id
-          };
-        });
-    this.setState({ requestSent: false });
-    this.setState(info.data);
-  };
+  // componentWillUpdate = async () => {
+  //   this.setState({ requestSent: true });
+  //   const info = await axios.get(URL + "/user/getInfo?_id=" + this.state._id);
+  //   if (info.data === "NOT FOUND") {
+  //     history.goBack();
+  //     return;
+  //   }
+  //   if (info.data.id.length > 0)
+  //     await axios
+  //       .get(URL + "/user/subscriptions/?id=" + this.state.id)
+  //       .then(res => {
+  //         info.data.existingPlan = {
+  //           id: res.data.plan.id,
+  //           name: res.data.plan.nickname,
+  //           sub_id: res.data.sub_id
+  //         };
+  //       });
+  //   this.setState({ requestSent: false });
+  //   this.setState(info.data);
+  // };
 
   componentDidMount = async () => {
     this.setState({ requestSent: true });
